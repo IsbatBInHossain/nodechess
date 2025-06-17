e883110c35d374f31c0b70cad3af244ccb31c6eb
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-06-18 02:18:04 +0600

    feat: add auth services (loginAsGuest, login and register functions)

---
+++ src/api/auth.service.ts
@@ -0,0 +1,44 @@
+import axios from 'axios'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+if (!BASE_URL) {
+  throw new Error(
+    'VITE_API_BASE_URL is not defined in the environment variables'
+  )
+}
+
+export const loginAsGuest = async () => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/guest`)
+    return response.data
+  } catch (error) {
+    console.error('Error logging in as guest:', error)
+    throw error
+  }
+}
+
+export const login = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/login`, {
+      username,
+      password,
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error logging in:', error)
+    throw error
+  }
+}
+
+export const register = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/register`, {
+      username,
+      password,
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error registering:', error)
+    throw error
+  }
+}
